bc79f89df8978ed5ede28a3afaecb5955342d12e
Jeff Hitchcock <mail@command-center.org>
2021-10-29 12:18:19 -0700

    Update template & data to fix derived data issues

--- module/data/item/_module.mjs
+++ module/data/item/_module.mjs
@@ -31,3 +31,15 @@ export const config = {
   tool: ItemToolData,
   weapon: ItemWeaponData
 };
+
+/**
+ * Method for checking to ensure data definition properly matches document data.
+ * @param {Document} document         Document to use for comparison.
+ * @param {DocumentData} definition   Data definition to compare against.
+ * @param {boolean} compareToDefault  Compare document against defaults.
+ * @returns {object}
+ */
+export function _checkData(document, definition, compareToDefault=false) {
+  const otherData = compareToDefault ? {} : foundry.utils.deepClone(document.data._source.data);
+  return foundry.utils.diffObject(document.data._source.data, (new definition(otherData)).toObject(false));
+}
