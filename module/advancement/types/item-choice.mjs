893b638a71ebaf36f40d13715c68b06b0fa6db31
Jeff Hitchcock <mail@command-center.org>
2022-06-27 10:24:26 -0700

    [#1401] Add item choice config

--- module/advancement/types/item-choice.mjs
+++ module/advancement/types/item-choice.mjs
@@ -1,4 +1,5 @@
 import Advancement from "../advancement.mjs";
+import AdvancementConfig from "../advancement-config.mjs";
 
 /**
  * Advancement that presents the player with a choice of multiple items that they can take. Keeps track of which
@@ -15,14 +16,17 @@ export class ItemChoiceAdvancement extends Advancement {
         configuration: {
           choices: {},
           allowDrops: true,
-          pool: null
+          pool: []
         }
       },
       order: 50,
-      icon: "icons/svg/stone-path.svg",
+      icon: "systems/dnd5e/icons/svg/item-choice.svg",
       title: game.i18n.localize("DND5E.AdvancementItemChoiceTitle"),
       hint: game.i18n.localize("DND5E.AdvancementItemChoiceHint"),
-      multiLevel: true
+      multiLevel: true,
+      apps: {
+        config: ItemChoiceConfig
+      }
     });
   }
 
@@ -58,6 +62,35 @@ export class ItemChoiceAdvancement extends Advancement {
     const items = this.data.value[level];
     if ( !items ) return "";
     return Object.values(items).reduce((html, uuid) => html + game.dnd5e.utils.linkForUuid(uuid), "");
+  }
+
+}
+
+
+/**
+ * Configuration application for item choices.
+ *
+ * @extends {AdvancementConfig}
+ */
+export class ItemChoiceConfig extends AdvancementConfig {
+
+  /** @inheritdoc */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["dnd5e", "advancement", "item-choice", "two-column"],
+      dragDrop: [{ dropSelector: ".drop-target" }],
+      dropKeyPath: "pool",
+      template: "systems/dnd5e/templates/advancement/item-choice-config.hbs",
+      width: 540
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  prepareConfigurationUpdate(configuration) {
+    if ( configuration.choices ) configuration.choices = this.constructor._cleanedObject(configuration.choices);
+    return configuration;
   }
 
 }
